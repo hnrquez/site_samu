61f3a9eb5437ed20f95103a415e0ed51dfe8cd8d
Pedro Henrique <115908728+hnrquez@users.noreply.github.com>
2026-03-29 15:01:43 -0300

    Adicionar todos os arquivos do projeto

---
+++ scr.js
@@ -0,0 +1,6 @@
+document.getElementById("sim").addEventListener("click", function(){
+    window.location.href = "sim.html";
+});
+document.getElementById("nao").addEventListener("click", function(){
+    window.location.href="nao.html";
+});
